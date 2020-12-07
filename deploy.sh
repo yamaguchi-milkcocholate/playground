150d0ddf21fedc12b991cc62773039ab2f6bb706
Yamaguchi Teppei <zuuuubo.tetsu@outlook.jp>
2020-12-07 23:29:01 +0900

    [delete] redundant

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-app=playground-d
-
-heroku login
-heroku container:login
-docker build -t registry.heroku.com/$app/web .
-docker push registry.heroku.com/$app/web
-heroku container:release --app $app web
